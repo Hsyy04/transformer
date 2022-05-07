66adcd4049e59aa6e9d50cb56a586805cc37ce5c
hsyy04 <594659663@qq.com>
2022-05-07 16:55:04 +0800

    init

---
+++ main.py
@@ -0,0 +1,165 @@
+from torch import int32, int64, kl_div
+from sklearn.utils import shuffle
+from torch.optim.optimizer import Optimizer
+from transformer import Transformer
+from torch.utils.data import DataLoader,Dataset,random_split
+from torch import Tensor,nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch
+from datetime import datetime
+from tqdm import tqdm
+import os
+from torch.utils.tensorboard import SummaryWriter
+
+os.environ['CUDA_VISIBLE_DEVICES']='3'
+torch.cuda.empty_cache()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using {device} device')
+
+class SentPairSet(Dataset):
+    def __init__(self, src_len, tar_len) -> None:
+        super().__init__()
+        with open('data/dict_en.txt','r', encoding='UTF-8') as f:
+            words_src_all=f.read().split('\n')
+        self.src_word2id = dict([(w,i) for i,w in enumerate(words_src_all)])
+        self.src_id2word = dict([(i,w) for i,w in enumerate(words_src_all)])
+        
+        with open('data/dict_ch.txt','r', encoding='UTF-8') as f:
+            words_tar_all = f.read().split('\n')
+        self.tar_word2id = dict([(w,i) for i,w in enumerate(words_tar_all)])
+        self.tar_id2word = dict([(i,w) for i,w in enumerate(words_tar_all)])
+
+        with open("data/sent_pairs.txt", 'r', encoding='UTF-8') as f:
+            data_raw = f.read().split('\n')
+        self.data_sent_pair = []
+        for sent in data_raw[-500:]:
+            sent_src = sent.split(' ')[0].split(' ')
+            sent_src_id = []
+            for wd in sent_src:
+                if wd in self.src_word2id:
+                    sent_src_id.append(self.src_word2id[wd])
+                else: 
+                    sent_src_id.append(self.src_word2id['<unk>'])
+            while(len(sent_src_id)<src_len): sent_src_id.append(0)
+        
+            sent_tar = sent.split(' ')[0].split(' ')
+            sent_tar_id = []
+            for wd in sent_tar:
+                if wd in self.tar_word2id:
+                    sent_tar_id.append(self.tar_word2id[wd])
+                else: 
+                    sent_tar_id.append(self.tar_word2id['<unk>'])
+            while(len(sent_tar_id)<src_len): sent_tar_id.append(0)
+
+            self.data_sent_pair.append([sent_src_id[:src_len], sent_tar_id[:tar_len]])
+        
+    def __getitem__(self, index):
+        return Tensor(self.data_sent_pair[index][0]).to(int32),Tensor(self.data_sent_pair[index][1]).to(int64)
+
+    def __len__(self):
+        return len(self.data_sent_pair)
+
+    def get_src_vocab_size(self):
+        return len(self.src_word2id)
+    def get_tar_vocab_size(self):
+        return len(self.tar_word2id)
+
+class LabelSmoothing(nn.Module):
+    # FIXME
+    def __init__(self, tar_len, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.tar_len = tar_len
+        self.true_dist = None
+
+    def forward(self, x, target):
+        # x: [N, seq_len, vocab_size]
+        # target: [N, seq_len]  this is true
+        target  = target.fill(self.smoothing / (self.tar_len - 2))
+        target = target.scatter(1, target.data.unsqueeze(1), self.confidence)
+        target[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            target = target.index_fill(0, mask.squeeze(), 0.0)
+
+        return self.criterion(x, Variable(target, requires_grad=False))
+
+def forward_and_loss(model, data_batch, mode, optimizer:Optimizer=None, smooth_loss=None):
+    if mode == "train":
+        model.train()
+    else: model.eval()
+    tot_loss = 0.0
+    for src, tar in tqdm(data_batch, mininterval=10):
+        src, tar = src.to(device), tar.to(device)
+        output = model(src, tar)
+        if smooth_loss is not None:
+            loss = smooth_loss(output, tar)
+            tot_loss += loss
+        else: 
+            N, len, vocab= output.shape
+            tar_p = torch.zeros((N, len, vocab), dtype=torch.float, device=device).scatter_(dim=-1, index=tar.unsqueeze(-1), value=1)
+            loss = F.kl_div(output, tar_p)
+            tot_loss += loss
+        if mode == "train":
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    return tot_loss
+if __name__ == "__main__":
+    LEARNING_RATE = 0.00003
+    EPOCH_NUM = 40
+    BATCH_SIZE= 1
+    HEAD_NUM = 4
+    LAYER_NUM = 2
+    DMODEL = 256
+    TAR_LEN = 128
+    SRC_LEN = 128
+    currentTime = datetime.now().strftime('%b%d_%H-%M-%S')
+    NAME = f'model({LAYER_NUM}_{LAYER_NUM}_{DMODEL})_lr{LEARNING_RATE}_en{EPOCH_NUM}_bsz{BATCH_SIZE}'
+    # data
+    data_set = SentPairSet(SRC_LEN, TAR_LEN)
+    train_len = int(len(data_set)*0.8)
+    train_dataset, test_dataset = random_split(
+        dataset=data_set,
+        lengths=[train_len, len(data_set)-train_len],
+    )
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+    # model and optimizer
+    model = Transformer(
+        device, 1024, 
+        data_set.get_src_vocab_size(), 
+        data_set.get_tar_vocab_size(),
+        nlayer=LAYER_NUM,
+        nhead=HEAD_NUM,
+        dmodel=DMODEL
+    )
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    #? smooth loss
+    # FIXME: smooth_loss = LabelSmoothing(TAR_LEN, 0, 0.5)
+    # tensorboard
+    writer = SummaryWriter(f"runs/{NAME}_{currentTime}/")
+    # train
+    print("start trainning...")
+    for epoch in range(EPOCH_NUM):
+        model.train()
+        print(f"Epoch: {epoch+1}/{EPOCH_NUM}")
+
+        totloss = forward_and_loss(model, train_dataloader,'train',optimizer)
+
+        print(f"loss:{totloss}")
+        writer.add_scalars('train/loss', {'loss':totloss}, epoch)
+        if epoch % 4 == 0:
+            test_loss = forward_and_loss(model, test_dataloader, 'test')
+            dic = {'test':test_loss}
+            writer.add_scalars('result/loss', dic, epoch)
+            torch.save(model, f"model/{NAME}.pth") 
+            print("saving...")
+        print('\n')
+    # TODO: bleu score
+    pass
---
+++ transformer.py
@@ -0,0 +1,177 @@
+from torch import Tensor, nn
+import torch.nn.functional as F
+import torch
+import math
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, nhead=8, dmodel=512):
+        super().__init__()
+        dk = dmodel//nhead
+        self.nhead = nhead
+        self.dmodel = dmodel
+    
+        self.WQ = torch.nn.Parameter(torch.rand((nhead,dmodel,dk),requires_grad=True, device=torch.device('cuda:0')))
+        self.WK = torch.nn.Parameter(torch.rand((nhead,dmodel,dk),requires_grad=True, device=torch.device('cuda:0')))
+        self.WV = torch.nn.Parameter(torch.rand((nhead,dmodel,dk),requires_grad=True, device=torch.device('cuda:0')))
+        self.WO = nn.Linear(dk*nhead, dmodel)
+
+        self.init_weights()
+    
+    def init_weights(self):
+        initrange = 0.1
+        self.WQ.data.uniform_(-initrange, initrange)
+        self.WK.data.uniform_(-initrange, initrange)
+        self.WV.data.uniform_(-initrange, initrange)
+
+    def forward(self, K:Tensor, Q:Tensor, V:Tensor, mask:Tensor):
+        #  Q: [N, Q_len, dmodel]
+        #  K: [N, K_len, dmodel]
+        #  V: [N, V_len, dmodel]
+        #  K_len == V_len
+        N, q_len, dmodel = Q.shape
+        k_len = K.shape[1]
+        Q = torch.unsqueeze(Q,1)            #  Q: [N, 1, Q_len, dmodel]
+        K = torch.unsqueeze(K,1)            #  K: [N, 1, K_len, dmodel]
+        V = torch.unsqueeze(V,1)            #  V: [N, 1, V_len, dmodel]
+        
+        Qi:Tensor= Q @ self.WQ              # Qi: [N, nheads, Q_len, dk]
+        Ki:Tensor = K @ self.WK             # Ki: [N, nheads, K_len, dk]
+        Vi:Tensor = V @ self.WV             # Vi: [N, nheads, V_len, dk]
+        dk = Qi.shape[-1]
+
+        energy = Qi@Ki.contiguous().transpose(-2,-1) # [N, nheads, Q_len, K_len]
+
+        if mask is not None:
+            energy = energy.masked_fill(mask==0, -1e20)
+
+        headi = F.softmax( energy / math.sqrt(float(dk)), dim=-1) # headi: [N, nheads, Q_len, K_len]
+        headi:Tensor = headi@Vi                # [N, nheads, Q_len, dk]  (ps. k_len==v_len, 在该维度计算)
+        output = self.WO(headi.transpose(1,2).reshape(N, q_len, dk*self.nhead))
+        return output           #[N, q_len, dmodel]
+
+class PosEncoder(nn.Module):
+
+    def __init__(self, dmodel: int, dropout: float = 0.1, max_len: int = 515):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, dmodel, 2) * (-math.log(10000.0) / dmodel))
+        pe = torch.zeros(max_len, dmodel)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [N, seq_len, embedding_dim]
+        pe : [max_len, embedding_dim]
+        """
+        x = x + self.pe[:x.size(1)]
+        return self.dropout(x)
+
+class EncoderLayer(nn.Module):
+    def __init__(self, nhead=8, dmodel=512, forward_expansion=4, p=0.1):
+        super().__init__()
+        self.attention = MultiHeadAttention(nhead, dmodel)
+        self.norm1 = nn.LayerNorm(dmodel)
+        self.norm2 = nn.LayerNorm(dmodel)
+        self.FFnet = nn.Sequential(
+            nn.Linear(dmodel, forward_expansion*dmodel),
+            nn.Linear(forward_expansion*dmodel, dmodel)
+        )
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x, mask):
+        at = self.dropout(self.attention(x,x,x,mask))
+        x = self.norm1(at+x)
+
+        at = self.dropout(self.FFnet(x))
+        x = self.norm2(at+x)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, nlayer=6, nhead=8, dmodel=512, forward_expansion=4, p=0.1, max_len:int=515):
+        super().__init__()
+        self.word_emb = nn.Embedding(src_vocab_size, dmodel)
+        self.add_pos= PosEncoder(dmodel=dmodel,max_len=max_len)
+        self.layers = nn.ModuleList([EncoderLayer(nhead, dmodel, forward_expansion, p) for i in range(nlayer)])
+    
+    def forward(self, x, mask=None):
+        # x: [N, seq_len]
+        x = self.add_pos(self.word_emb(x))  # x:[N, seq_len, dmodel]
+        for layer in self.layers:
+            x = layer(x, mask)
+        return x
+
+class DecoderLayer(nn.Module):
+    def __init__(self, nhead=8, dmodel=512, forward_expansion=4, p=0.1):
+        super().__init__()
+        self.attention_masked = MultiHeadAttention(nhead, dmodel)
+        self.norm1 = nn.LayerNorm(dmodel)
+
+        self.attention= MultiHeadAttention(nhead, dmodel)
+        self.norm2 = nn.LayerNorm(dmodel)
+
+        self.FFnet = nn.Sequential(
+            nn.Linear(dmodel, forward_expansion*dmodel),
+            nn.ReLU(),
+            nn.Linear(forward_expansion*dmodel, dmodel),
+            nn.ReLU(),
+        )
+        self.norm3 = nn.LayerNorm(dmodel)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, k, q, v, src_mask, tar_mask):
+        at = self.dropout(self.attention_masked(q, q, q, tar_mask))
+        x = self.norm1(at+q)
+
+        at = self.dropout(self.attention(k, x, v, src_mask))
+        x = self.norm2(at+x)
+
+        at = self.dropout(self.FFnet(x))
+        x = self.norm3(at+x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, tar_vocab_size, nlayer=6, nhead=8, dmodel=512, forward_expansion=4, p=0.1, max_len:int=515):
+        super().__init__()
+        self.word_emb = nn.Embedding(tar_vocab_size, dmodel)
+        self.add_pos= PosEncoder(dmodel=dmodel,max_len=max_len)
+        self.layers = nn.ModuleList([DecoderLayer(nhead, dmodel, forward_expansion, p) for i in range(nlayer)])
+        self.output = nn.Linear(dmodel, tar_vocab_size) #? 这个参数要跟共享吗？？？？
+    
+    def forward(self, q, enc, src_mask, tar_mask):
+        # q: [N, seq_len]
+        q = self.add_pos(self.word_emb(q))  # q:[N, seq_len, dmodel]
+        for layer in self.layers:
+            q = layer(enc, q, enc, src_mask, tar_mask)
+        q = self.output(q)   # q:[N, seq_len, tar_vocab_size]
+        return F.log_softmax(q, dim=-1)
+
+class Transformer(nn.Module):
+    def __init__(self, device, max_len, src_vocab_size, tar_vocab_size, pad_id=0, nlayer=6, nhead=8, dmodel=512, forward_expansion=4, p=0.1):
+        super().__init__()
+        self.encoder = Encoder(src_vocab_size, nlayer, nhead, dmodel, forward_expansion, p, max_len)
+        self.decoder = Decoder(tar_vocab_size, nlayer, nhead, dmodel, forward_expansion, p, max_len)
+        self.device = device
+        self.pad_id = pad_id
+
+    def make_tar_mask(self, tar:Tensor):
+        N, tar_len = tar.shape
+        tar_mask = torch.tril(torch.ones(tar_len, tar_len)).unsqueeze(0).expand(N, -1, -1).unsqueeze(1).to(self.device)
+        # tar_mask [N, 1, tar_len, tar_len]
+        return tar_mask
+
+    def make_src_mask(self, src:Tensor):
+        N, src_len = src.shape
+        src_mask = (src!=self.pad_id).unsqueeze(1).unsqueeze(1)       # [N, 1, 1, src_len]
+        return src_mask
+
+    def forward(self, src, tar):
+        src_mask = self.make_src_mask(src)
+        tar_mask = self.make_tar_mask(tar)
+        enc = self.encoder(src, src_mask)
+        out = self.decoder(tar, enc, src_mask, tar_mask) # [N, seq_len, tar_vocab_size]
+        return out
